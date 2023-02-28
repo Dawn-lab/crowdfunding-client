1be008af100a14f3b16a9fdce44f99bca694b27e
Dawn-lab <julius.githinji@outlook.com>
2023-02-28 10:34:48 +0300

    client initialization

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h1>My name is Julius Githinji</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App

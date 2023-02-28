8bdb6e0d4d875791ab9ad81bee5d3d9ec1c4367f
Dawn-lab <julius.githinji@outlook.com>
2023-02-28 23:56:34 +0300

    Pages initialization and components initialization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import CreateCampaign from './pages/CreateCampaign'
+import CampaignDetails from './pages/CampaignDetails'
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
+
 
 function App() {
 
   return (
-    <div className="App">
-      <div>
-        <h1>My name is Julius Githinji</h1>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar/>
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar/>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
       </div>
     </div>
+
   )
 }
 
